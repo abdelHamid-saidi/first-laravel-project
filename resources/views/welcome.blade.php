bfbcd662d8c605eed051c7e6892ac9084a7bc889
Abdelhamid Saidi <saidiabdelhamid.office@gmail.com>
2026-03-11 10:18:16 +0100

    ACTIVITE 12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,19 @@
 @section('title', 'Accueil')
 
 @section('content')
+
 <h2>Bienvenue sur le site de {{ $name }}</h2>
 
-<x-article
-  title="L’IA soigne mieux"
-  description="L’intelligence artificielle aide les médecins à diagnostiquer plus vite." />
+@forelse ($articles as $article)
 
 <x-article
-  title="Villes vertes"
-  description="Les métropoles deviennent plus écologiques et durables." />
+  :title="$article['title']"
+  :description="Str::limit($article['description'], 30)" />
 
-<x-article
-  title="Télétravail"
-  description="Plus de liberté, mais aussi plus de solitude." />
+@empty
+
+<p>Aucun article pour le moment.</p>
+
+@endforelse
+
 @endsection
